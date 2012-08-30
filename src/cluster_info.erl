04a10721def92e2eafd35363af6cf96eae423db2
Evan Vigil-McClanahan <mcclanahan@gmail.com>
2012-08-30 12:11:15 -0700

    Pull in scott's HTML changes, plus change the application to not expect the code to live on the other side, but to do all of its information-gathering via standard rpc.
    There is some partial dead-code removal in here, I don't know how
    far we want to diverge from the base repo.  There's some other
    stuff that I haven't touched but suspect won't work now.

--- src/cluster_info.erl
+++ src/cluster_info.erl
@@ -37,13 +37,8 @@
          dump_all_connected/1, dump_all_connected/2,
          dump_nodes/2, dump_nodes/3,
          list_reports/0, list_reports/1,
-         send/2,
-         format/2, format/3,
-         format_noescape/2, format_noescape/3]).
+         format/2, format/3]).
 -export([get_limits/0, reset_limits/0]).
-
-%% Really useful but ugly hack.
--export([capture_io/2]).
 
 -type dump_option() :: {'modules', [atom()]}.
 -type dump_return() :: 'ok' | 'error'.
@@ -110,21 +105,14 @@ dump_node(Node, Path) ->
 dump_node(Node, Path, Opts) when is_atom(Node), is_list(Path) ->
     io:format("Writing report for node ~p\n", [Node]),
     {ok, FH} = file:open(Path, [append]),
-    Collector = self(),
-    Remote = spawn(Node, fun() ->
-                                 dump_local_info(Collector, Opts),
-                                 collector_done(Collector)
-                         end),
-    {ok, MRef} = gmt_util_make_monitor(Remote),
     Res = try
-              ok = collect_remote_info(Remote, FH)
+              collect_remote_info(Node, Opts, FH)
           catch X:Y ->
                   io:format("Error: ~P ~P at ~p\n",
                             [X, 20, Y, 20, erlang:get_stacktrace()]),
                   error
           after
-              catch file:close(FH),
-              gmt_util_unmake_monitor(MRef)
+              catch file:close(FH)
           end,
     Res.
 
@@ -182,17 +170,11 @@ list_reports(Mod) ->
                                 Filter(M),
                                 {Name, _} <- GetGens(M)]).
 
-format(Pid, Fmt) ->
-    format(Pid, Fmt, []).
+format(FH, Fmt) ->
+    format(FH, Fmt, []).
 
-format(Pid, Fmt, Args) ->
-    send(Pid, safe_format(Fmt, Args)).
-
-format_noescape(Pid, Fmt) ->
-    format_noescape(Pid, Fmt, []).
-
-format_noescape(Pid, Fmt, Args) ->
-    send2(Pid, safe_format(Fmt, Args)).
+format(FH, Fmt, Args) ->
+    io:format(FH, Fmt, Args).
 
 %%----------------------------------------------------------------------
 %% Func: stop/1
@@ -206,34 +188,13 @@ stop(_State) ->
 %%% Internal functions
 %%%----------------------------------------------------------------------
 
-collect_remote_info(Remote, FH) ->
-    receive
-        {'DOWN', _, X, Remote, Z} ->
-            io:format("Remote error: ~p ~p (~p)\n    -> ~p\n",
-                      [X, Remote, node(Remote), Z]),
-            ok;
-        {collect_data, Remote, IoList} ->
-            file:write(FH, IoList),
-            collect_remote_info(Remote, FH);
-        {collect_data_ack, Remote} ->
-            Remote ! collect_data_goahead,
-            collect_remote_info(Remote, FH);
-        {collect_done, Remote} ->
-            ok
-    after 120*1000 ->
-            timeout
-    end.
-
-collector_done(Pid) ->
-    Pid ! {collect_done, self()}.
-
-dump_local_info(CPid, Opts) ->
-    dbg("D: node = ~p\n", [node()]),
-    format(CPid, "\n"),
-    format_noescape(CPid, "<a name=\"~p\">\n", [node()]),
-    format_noescape(CPid, "<h1>Local node cluster_info dump, Node: ~p</h1>\n", [node()]),
-    format(CPid, "   Options: ~p\n", [Opts]),
-    format(CPid, "\n"),
+collect_remote_info(Node, Opts, FH) ->
+    dbg("D: node = ~p\n", [Node]),
+    format(FH, "\n"),
+    format(FH, "<a name=\"~p\">\n", [Node]),
+    format(FH, "<h1>Local node cluster_info dump, Node: ~p</h1>\n", [Node]),
+    format(FH, "   Options: ~p\n", [Opts]),
+    format(FH, "\n"),
     Mods0 = lists:sort([Mod || {Mod, _Path} <- code:all_loaded()]),
     Filters = proplists:get_value(modules, Opts, [all]),
     Mods = [M || M <- Mods0,
@@ -244,85 +205,39 @@ dump_local_info(CPid, Opts) ->
          {'EXIT', _} ->
              ok;
          NameFuns when is_list(NameFuns) ->
-             format_noescape(CPid, "<ul>\n", []),
+             format(FH, "<ul>\n", []),
              [begin
-                  A = make_anchor(node(), Mod, Name),
-                  format_noescape(
-                    CPid, "<li> <a href=\"#~s\">~s</a>\n", [A, Name])
+                  A = make_anchor(Node, Mod, Name),
+                  format(
+                    FH, "<li> <a href=\"#~s\">~s</a>\n", [A, Name])
               end || {Name, _} <- NameFuns],
-             format_noescape(CPid, "<ul>\n", []),
+             format(FH, "<ul>\n", []),
              [try
                   dbg("D: generator ~p ~s\n", [Fun, Name]),
-                  format_noescape(CPid, "\n<a name=\"~s\">\n",
-                                  [make_anchor(node(), Mod, Name)]),
-                  format_noescape(CPid, "<h2>Report: ~s (~p)</h2>\n\n",
-                                  [Name, node()]),
-                  format_noescape(CPid, "<pre>\n", []),
-                  Fun(CPid)
+                  format(FH, "\n<a name=\"~s\">\n",
+                                  [make_anchor(Node, Mod, Name)]),
+                  format(FH, "<h2>Report: ~s (~p)</h2>\n\n",
+                                  [Name, Node]),
+                  format(FH, "<pre>\n", []),
+                  RC = fun(M, F, A) ->
+                               rpc:call(Node, M, F, A, 10000)
+                       end,
+                  Fun(FH, RC)
               catch X:Y ->
-                      format(CPid, "Error in ~p: ~p ~p at ~p\n",
+                      format(FH, "Error in ~p: ~p ~p at ~p\n",
                              [Name, X, Y, erlang:get_stacktrace()])
               after
-                  format_noescape(CPid, "</pre>\n", []),
-                  format(CPid, "\n")
+                  format(FH, "</pre>\n", []),
+                  format(FH, "\n")
               end || {Name, Fun} <- NameFuns]
      end || Mod <- Mods],
     ok.
 
 dbg(_Fmt, _Args) ->
-    ok.
+   ok.
 
-%%%%%%%%%
-
-%% @doc This is an untidy hack.
-
--spec capture_io(integer(), function()) -> [term()].
-capture_io(Timeout, Fun) ->
-    Me = self(),
-    spawn(fun() -> capture_io2(Timeout, Fun, Me) end),
-    lists:flatten(harvest_reqs(Timeout)).
-
-capture_io2(Timeout, Fun, Parent) ->
-    group_leader(self(), self()),
-    Fudge = 50,
-    Worker = spawn(fun() -> Fun(), timer:sleep(Fudge), Parent ! io_done2 end),
-    spawn(fun() -> timer:sleep(Timeout + Fudge + 10), exit(Worker, kill) end),
-    get_io_reqs(Parent, Timeout).
-
-get_io_reqs(Parent, Timeout) ->
-    receive
-        {io_request, From, _, Req} ->
-            From ! {io_reply, self(), ok},
-            Parent ! {io_data, Req},
-            get_io_reqs(Parent, Timeout)
-    after Timeout ->
-            ok
-    end.
-
-harvest_reqs(Timeout) ->
-    receive
-        {io_data, Req} ->
-            case Req of
-                {put_chars, _, Mod, Fun, Args} ->
-                    [erlang:apply(Mod, Fun, Args)|harvest_reqs(Timeout)];
-                {put_chars, _, Chars} ->
-                    [Chars|harvest_reqs(Timeout)]
-            end;
-        io_done ->
-            [];
-        io_done2 ->
-            []
-    after Timeout ->
-            []
-    end.
-
-safe_format(Fmt, Args) ->
-    case get_limits() of
-        {undefined, _} ->
-            io_lib:format(Fmt, Args);
-        {TermMaxSize, FmtMaxBytes} ->
-            riak_err_handler:limited_fmt(Fmt, Args, TermMaxSize, FmtMaxBytes)
-    end.
+%% dbg(Fmt, Args) ->
+%%     io:format(Fmt, Args).
 
 get_limits() ->
     case erlang:get(?DICT_KEY) of
@@ -363,74 +278,3 @@ make_anchor(Node0, Mod, Name) ->
     Node = re:replace(atom_to_list(Node0), "'", "", [{return, list}, global]),
     lists:flatten(io_lib:format("~s~w~s",
                                 [Node, Mod, NameNoSp])).
-
-%% From gmt_util.erl, also Apache Public License'd.
-
-%% @spec (server_spec()) -> {ok, monitor_ref()} | error
-%% @doc Simplify the arcane art of <tt>erlang:monitor/1</tt>:
-%%      create a monitor.
-%%
-%% The arg may be a PID or a {registered_name, node} tuple.
-%% In the case of the tuple, we will use rpc:call/4 to find the
-%% server's actual PID before calling erlang:monitor();
-%% therefore there is a risk of blocking by the RPC call.
-%% To avoid the risk of blocking in this case, use make_monitor/2.
-
-gmt_util_make_monitor(Pid) when is_pid(Pid) ->
-    case catch erlang:monitor(process, Pid) of
-        MRef when is_reference(MRef) ->
-            receive
-                {'DOWN', MRef, _, _, _} ->
-                    error
-            after 0 ->
-                    {ok, MRef}
-            end;
-        _ ->
-            error
-    end.
-
-%% @spec (pid()) -> {ok, monitor_ref()} | error
-%% @doc Simplify the arcane art of <tt>erlang:demonitor/1</tt>:
-%%      destroy a monitor.
-
-gmt_util_unmake_monitor(MRef) ->
-    erlang:demonitor(MRef),
-    receive
-        {'DOWN', MRef, _, _, _} ->
-            ok
-    after 0 ->
-            ok
-    end.
-
-send(Pid, IoData) ->
-    ReList = [{"&", "\\&amp;"}, {"<", "\\&lt;"}],
-    Str = lists:foldl(fun({RE, Replace}, Str) ->
-                              re:replace(Str, RE, Replace, [{return,binary},
-                                                            global])
-                      end, IoData, ReList),
-    send2(Pid, Str).
-
-send2(Pid, IoData) ->
-    Pid ! {collect_data, self(), IoData},
-    case incr_counter() rem 10 of
-        0 ->
-            Pid ! {collect_data_ack, self()},
-            receive
-                collect_data_goahead ->
-                    ok
-            after 15*1000 ->
-                    ok
-            end;
-        _ ->
-            ok
-    end.
-
-incr_counter() ->
-    Key = {?MODULE, incr_counter},
-    case erlang:get(Key) of
-        undefined ->
-            erlang:put(Key, 1),
-            0;
-        N ->
-            erlang:put(Key, N+1)
-    end.
