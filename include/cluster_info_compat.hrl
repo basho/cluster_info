0cf46211437baae1a06d9e068dbccd0f0c3be674
Rick Chen <rick.chen@zoom.us>
2019-10-23 17:38:12 +0800

    minor fix

--- include/cluster_info_compat.hrl
+++ include/cluster_info_compat.hrl
@@ -1,6 +1,6 @@
 -ifndef(CLUSTER_INFO_COMPAT_HRL).
 
--ifdef(OTP_RELEASE). %% this implies OTP 2-1 or higher
+-ifdef(OTP_RELEASE). %% this implies OTP 21 or higher
 -define(COMPAT_CATCH(Class, Reason, Stacktrace), Class:Reason:Stacktrace).
 -define(COMPAT_GET_STACKTRACE(Stacktrace), Stacktrace).
 -else.
